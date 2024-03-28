1fc1bb34be8f1962b2e69d4a27be442ef5fdea1d
Clark <osborneclark5@gmail.com>
2024-03-28 19:18:29 -0400

    Fixed Movement/Following Camera
    Simple Autosave

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    void LateUpdate () 
+    {
+        if (transform.position != target.position) 
+        {
+            Vector3 targetPosition = new Vector3(target.position.x,target.position.y,transform.position.z);
+            transform.position = Vector3.Lerp(transform.position,targetPosition, smoothing);
+        }
+    }
+}
