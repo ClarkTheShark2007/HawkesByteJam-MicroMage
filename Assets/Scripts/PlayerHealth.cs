13bb4733a6eed42d178caafc7d65c0a5aef52351
Clark <osborneclark5@gmail.com>
2024-03-30 15:57:07 -0400

    Health/Gameover

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,21 +5,37 @@ using System;
 
 public class PlayerHealth : MonoBehaviour
 {
+
     public static event Action OnPlayerDamaged;
     public static event Action OnPlayerDeath;
 
     public int maxHealth;
     public int currentHealth;
 
+    private Animator anim;
+
+    AudioSource aud;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        OnPlayerDamaged?.Invoke();
+        anim = GetComponent<Animator>();
+        aud = GetComponent<AudioSource>();
     }
 
     public void TakeDamage(int damage)  
     {
         currentHealth -= damage;
         OnPlayerDamaged?.Invoke();
+
+        if (currentHealth <= 0) 
+        {
+            anim.SetTrigger("Gameover");
+            GetComponent<PlayerMovement>().enabled = false;
+            aud.Play();
+            OnPlayerDeath?.Invoke();
+        }
     }
 }
