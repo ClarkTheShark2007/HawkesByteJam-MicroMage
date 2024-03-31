88c5fa2c6f654699536df5a9d7f9755785927e4c
Clark <osborneclark5@gmail.com>
2024-03-31 13:30:27 -0400

    MainMenu

--- Assets/Scripts/ScreenSizeMenu.cs
+++ Assets/Scripts/ScreenSizeMenu.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScreenSize : MonoBehaviour
+public class ScreenSizeMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         Screen.SetResolution(800,600,true);
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         Debug.Log("WorksResolution");
     }
 }
