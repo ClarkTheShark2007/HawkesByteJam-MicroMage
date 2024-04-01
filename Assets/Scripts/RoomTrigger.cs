15c1e2fea54e412e064191f757274e73a60ad9d3
Clark <osborneclark5@gmail.com>
2024-03-31 22:52:56 -0400

    Working

--- Assets/Scripts/RoomTrigger.cs
+++ Assets/Scripts/RoomTrigger.cs
@@ -6,8 +6,16 @@ public class RoomTrigger : MonoBehaviour
 {
     public GameObject Walls;
 
+    AudioSource aud;
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         Walls.SetActive(true);
+        aud.Play();
     }
 }
