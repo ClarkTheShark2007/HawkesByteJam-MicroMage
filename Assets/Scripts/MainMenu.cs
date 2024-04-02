1c08e690aeccf00720e66c22f20440f6b36bb01c
Clark <osborneclark5@gmail.com>
2024-04-02 17:22:54 -0400

    UiWorks

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void PlayGame ()
     {
-        SceneManager.LoadScene("BryanScene");
+        SceneManager.LoadScene("LvlOne");
     }
 
     public void QuitGame () 
--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Scripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
