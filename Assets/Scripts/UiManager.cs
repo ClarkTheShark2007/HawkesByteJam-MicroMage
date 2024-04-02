1f35b990231b1ac4a8f35d80cbca54e7f6d6a512
Clark <osborneclark5@gmail.com>
2024-04-02 16:19:48 -0400

    E

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -7,9 +7,26 @@ using UnityEngine.SceneManagement;
 public class UiManager : MonoBehaviour
 {
     public GameObject gameOverMenu;
+    public GameObject levelScreen;
+
+    public GameObject Player;
 
     public AudioSource bossTheme;
     public AudioSource mainTheme;
+
+    void Start() 
+    {
+        levelScreen.SetActive(true);
+        Player.SetActive(false);
+        StartCoroutine(WaitToLoad());
+    }
+
+    IEnumerator WaitToLoad() 
+    {
+        yield return new WaitForSeconds(3);
+        levelScreen.SetActive(false);
+        Player.SetActive(true);
+    }
 
 
     private void OnEnable() 
