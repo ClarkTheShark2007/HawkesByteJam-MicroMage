151f34d64ea98f8ed780429264c45b763a4edb0c
Clark <osborneclark5@gmail.com>
2024-04-02 23:51:36 -0400

    Working

--- Assets/EnemyProjectileHeatSeaking.cs
+++ Assets/EnemyProjectileHeatSeaking.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyProjectile : MonoBehaviour
+public class EnemyProjectileHeatSeaking : MonoBehaviour
 {
 
     public float speed;
@@ -22,7 +22,7 @@ public class EnemyProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
 
         if(transform.position.x == target.x && transform.position.y == target.y) 
         {
--- Assets/EnemyShoot.cs
+++ Assets/EnemyShoot.cs
@@ -10,6 +10,8 @@ public class EnemyShoot : MonoBehaviour
     public float stoppingDistance;
     public float retreatDistance;
 
+    public float chaseRaidus;
+
     private float TimeBtwShots;
 
     public float StartTimeBtwShots;
@@ -19,27 +21,41 @@ public class EnemyShoot : MonoBehaviour
     public Transform player;
     public GameObject projectile;
 
+        public float attackRadius;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
         TimeBtwShots = StartTimeBtwShots;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, player.position) > stoppingDistance) 
+
+        CheckDistance();
+        
+    void CheckDistance () 
+    {
+        if(Vector3.Distance(player.position, transform.position) <= chaseRaidus && Vector3.Distance(player.position, transform.position) > attackRadius) 
+        {
+            if (Vector2.Distance(transform.position, player.position) > stoppingDistance) 
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
 
         } else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance) 
         {
             transform.position = this.transform.position;
+            
         } else if(Vector2.Distance(transform.position, player.position) < retreatDistance) 
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
         }
+        }
+    
 
         if(TimeBtwShots <= 0) {
 
@@ -53,5 +69,6 @@ public class EnemyShoot : MonoBehaviour
             }
         }
     }
+}
 
 
